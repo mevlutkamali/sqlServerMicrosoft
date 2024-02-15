7f8d340de1594ccaae52f1d05cbdd7a140cfcd3e
Mevlüt KAMALI <116831651+mevlutkamali@users.noreply.github.com>
2024-02-15 11:57:17 +0300

    sqlServerMicrosoft

---
+++ 13.0_innerJoin.sql
@@ -0,0 +1,5 @@
+SELECT * FROM studentsTable A INNER JOIN studentsAdress B ON A.StdudentNumber = B.StudentNumber;
+
+/*
+	" INNER JOIN " ile SQL'de ortak kesiþim noktalarýný elde edebiliriz.
+*/
---
+++ SQLQuery1.sql
@@ -0,0 +1,6 @@
+
+SELECT * FROM studentsTable
+
+SELECT * FROM studentsAdress
+
+/*SELECT * FROM studentsTable a INNER JOIN studentsAdress b ON a.StudentNumber = b.StudentNumber;*/
